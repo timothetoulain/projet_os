b1b38ad6075249b1093ed26528ec28971a2eed4a
timothetoulain <35801169+timothetoulain@users.noreply.github.com>
2019-03-24 11:45:35 +0100

    Delete cpdir.cpp

--- cpdir.cpp
+++
@@ -1,9 +0,0 @@
-//g++ cpdir.cpp -o cpdir -lstdc++fs
-#include <iostream>
-#include <fstream>
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
- 
-int main(int argc, char** argv){
-    fs::copy(argv[1], argv[2], fs::copy_options::recursive);
-}
